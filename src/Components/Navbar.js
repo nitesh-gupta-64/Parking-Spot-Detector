29b238dd49e3080f73af5b059c40e0d46dbaff39
nitesh-gupta-64 <123109031@nitkkr.ac.in>
2024-04-26 10:45:59 +0530

    Done

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-        <h1 className=" text-6xl font-bold m-3 p-3">Parking Spot Detector</h1>
+        <h1 className=" md:text-6xl text-4xl font-bold m-3 p-3">Parking Spot Detector</h1>
     </div>
   )
 }
